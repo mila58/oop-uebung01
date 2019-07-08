e470e20fdb70af73ab4c30db7181f49e632d8860
mila58 <lang_miriam@yahoo.de>
2019-07-08 21:15:25 +0200

    loesung

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,31 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+
+
+    private CharElement top;
+
+    public void push (char c){
+        top = new CharElement(c,top);
+    }
+
+    public char pop (){
+        if (top == null)
+                throw new NoSuchElementException();
+        char v = top.value;
+        top = top.next;
+        return v;
+    }
+
+    public int size(){
+        int counter = 0;
+        CharElement it = top;
+         while (it != null){
+              counter++;
+             it = it.next;
+         }
+         return counter;
+    }
 }
--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -1,9 +1,30 @@
 package ueb01;
 
 class Palindrom {
-	static boolean istPalindrom(String s) {
-		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+	String s;
+
+	static boolean istPalindrom(String s) {
+		if (s.length() == 1)
+			return true;
+
+		CharStack stack = new CharStackImpl();
+		char [] cs = s.toLowerCase().replaceAll(" ","").toCharArray();
+
+		// die erste Hälfte auf den Stack legen
+		int i = 0;
+		for (; i < cs.length/2; i++)
+			stack.push(cs[i]);
+
+
+		// ungerade Länge: ignoriere Buchstaben in der Mitte
+		if(cs.length % 2 == 1)
+			i++;
+
+		for (; i< cs.length; i++)
+			if(stack.pop() != cs[i])
+				return false;
+
+		return true;
 	}
 }
